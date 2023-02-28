a73b5b9e5ec72a0c0c1c6fb807a32a495b0fd473
Destin <berniedestin7@gmail.com>
2023-02-28 16:29:19 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num < 0) {
+        return 'ERROR'
+    } else if ( num === 0) {
+        return ''
+    } else if ( num === 1 ) {
+        return str
+    }else {
+        let newStr = str
+        for ( let i = 1; i < num; i++){
+            newStr += str
+        }
+        return newStr
+    }
 };
 
 // Do not edit below this line
