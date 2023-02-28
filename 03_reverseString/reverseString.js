b2d430fbd19191d869540bea6798f72d59980f57
Destin <berniedestin7@gmail.com>
2023-02-28 16:42:14 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = ''
+    for ( let i = str.length; i > 0; i--) {
+        revStr += str.slice( i - 1, i)
+    }
+    return revStr
 };
 
 // Do not edit below this line
