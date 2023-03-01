8fdd829aed7290c011a9e4c906863a3b4811c22c
Destin <berniedestin7@gmail.com>
2023-03-01 10:01:24 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
-
+    let testArray = arguments[0]
+    //console.log("initial " + testArray)
+    for ( let i = 1; i < arguments.length; i++) {
+        // for ( const extract of testArray) {
+        for ( let j = 0; j < testArray.length; j++) {
+            //console.log("inside j loop " + testArray)
+            if ( testArray[j] === arguments[i]) {
+                testArray.splice(j,1)
+                //console.log("inside if " + testArray)
+            }
+        }
+    }
+    return testArray
 };
 
 // Do not edit below this line
