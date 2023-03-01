caa07d6469998bbcd87e1b93571631d130f51e2e
Destin <berniedestin7@gmail.com>
2023-03-01 10:54:35 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInput) {
+  let tempToCelsius = (( tempInput - 32 ) * (5 / 9))
+  return Math.round(tempToCelsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInput) {
+  let tempToFahrenheit = ((tempInput * (9 / 5)) +32)
+  return Math.round(tempToFahrenheit * 10) / 10
 };
 
 // Do not edit below this line
