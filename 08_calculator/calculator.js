c6bb23db2216c64bc2a89d09d086fabeb83e5e60
Destin <berniedestin7@gmail.com>
2023-03-02 17:05:26 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
-	
+const add = function(numA,numB) {
+	return numA + numB
 };
 
-const subtract = function() {
-	
+const subtract = function(numA,numB) {
+	return numA - numB
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  //if ( array.length == 0) return 0; unnecessary
+  let sum = 0
+  array.forEach( num => {
+    sum += num
+  })
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let ans = 1
+  array.forEach( num => {
+    ans *= num
+  })
+  return ans
 };
 
-const power = function() {
-	
+const power = function(num,pow) {
+  let ans = num
+  for ( let i = 1; i < pow; i++) {
+    ans *= num
+  }
+	return ans
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0 ) {
+    return 1
+  } else {
+    let ans = num
+    for ( let i = 1; i < num; i++) {
+      ans *= i
+      }
+	  return ans
+  }
 	
 };
 
