51fc5cb31335c9fcd9821e234648ebbeed6f5bba
Destin <berniedestin7@gmail.com>
2023-03-02 17:57:44 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    // would normally check if type is string
+    // and if it just has one character
+    // but this doesn't need that so I won't
+    let finalArray = []
+    let arrayInput = input.split('')
+    let realPos = 0
+    for ( let char of arrayInput) {
+        if ( char.toUpperCase() != char.toLowerCase() ) {
+            finalArray[realPos] = char.toLowerCase()
+            realPos++
+        }
+    }
+
+    //console.table(finalArray)
+    //console.log(Math.floor(finalArray.length / 2))
+
+    let i = 0
+    while ( i <= Math.floor(finalArray.length / 2) ) {
+        //console.log( `${finalArray[i]} vs ${finalArray[finalArray.length - 1 -i]}`)
+        if ( finalArray[i] !== finalArray[finalArray.length - 1- i]) {
+            return false
+        }
+        i++
+    }
+
+    return true
+
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
