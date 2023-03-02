bcb03e82c5ae8e2d1fe21cb5f0ccfde70baacdb2
Destin <berniedestin7@gmail.com>
2023-03-02 18:31:52 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if ( input < 1) return "OOPS"
+    var array = []
+    for ( let i = 0; i < input; i++) {
+        if ( i === 0 ) {
+            array[0] = 1
+        } else if ( i === 1) {
+            array[1] = 1
+        } else {
+            array[i] = array[ i - 1 ] + array[ i - 2 ]
+        }
+    }
+    return array[input - 1 ]
 };
 
 // Do not edit below this line
