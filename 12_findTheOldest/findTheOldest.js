f9ae1ac484c09799d72b1781b92b2374b2be639f
Destin <berniedestin7@gmail.com>
2023-03-02 19:08:44 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrInput) {
+    const arrOldest = arrInput.sort( (a,b) => {
+        let aAge
+        let bAge
+        const thisYear = new Date().getFullYear()
+        //console.log(thisYear)
+        if ( !("yearOfDeath" in a) ) {
+            aAge = thisYear - a.yearOfBirth
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth
+        }
 
+        if ( !("yearOfDeath" in b) ) {
+            bAge = thisYear - b.yearOfBirth
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth
+        }
+
+        return aAge < bAge ? 1 : -1;
+    })
+    //console.table(arrOldest)
+    return arrOldest[0]
 };
 
 // Do not edit below this line
